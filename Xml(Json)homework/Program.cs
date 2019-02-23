b80647ffeb05e716de577f9cf4adfb89b7107dd5
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-02-23 16:20:13 +0600

    Добавьте файлы проекта.

---
+++ Xml(Json)homework/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xml_and_Json_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Введите адрес проживания: ");
+
+            Console.WriteLine("город: ");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("улица: ");
+            string street = Console.ReadLine();
+
+            Console.WriteLine("дом: ");
+            int houseNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("квартира: ");
+            int apartamentNum = int.Parse(Console.ReadLine());
+
+            ResidentialAddress residentialAddress = new ResidentialAddress(city, street, houseNum, apartamentNum);
+
+            Console.WriteLine("домашний телефон: ");
+            string housePhone = Console.ReadLine();
+
+            Console.WriteLine("рабочий телефон: ");
+            string workPhone = Console.ReadLine();
+
+            UserData userData1 = new UserData(name, surname, residentialAddress, housePhone, workPhone);
+
+            userData1.ShowUserData();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Xml(Json)homework/UserData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xml_and_Json_homework
+{
+    public struct ResidentialAddress
+    {
+        public string City { get; set; }
+        public string StreetName { get; set; }
+        public int HouseNumber { get; set; }
+        public int ApartmentNumber { get; set; }
+
+        public ResidentialAddress(string city, string streetName, int houseNumber, int apartmentNumber)
+        {
+            City = city;
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+            ApartmentNumber = apartmentNumber;
+        }
+    }
+    class UserData
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+
+        private ResidentialAddress _residentialAddress;
+
+        public string[] telephone;
+
+        public UserData(string name, string surname, ResidentialAddress residentialAddress, string homeNumber, string workNumber)
+        {
+            Name = name;
+            SurName = surname;
+            _residentialAddress = residentialAddress;
+
+            telephone = new string[] { homeNumber, workNumber };
+        }
+
+        public void ShowUserData()
+        {
+            Console.WriteLine("Имя: " + Name + "\n" +
+                "Фамилия: " + SurName + "\n" +
+                "Адрес проживания:\n" +
+                "   город: " + _residentialAddress.City + "\n" +
+                "   улица: " + _residentialAddress.StreetName + "\n" +
+                "   дом: " + _residentialAddress.HouseNumber + "\n" +
+                "   квартира: " + _residentialAddress.ApartmentNumber +
+                "\n телефоны: " + telephone[0] + ", " + telephone[1]);
+        }
+    }
+}
+
